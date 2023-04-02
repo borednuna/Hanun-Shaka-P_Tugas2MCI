9fec7e4f1e2b2868691799609e11a93555b4eaf1
borednuna <hanunshaka02@gmail.com>
2023-04-03 04:13:51 +0700

    feat: remove unused codes

--- config/server.js
+++ config/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
-const sesiRoute = require('../app/routes/sesiRoutes.js');
 const cors = require('cors');
-const globals = require('./globals.js');
-const { Sequelize } = require('sequelize'); 
 const port = 3000;
+
+const sesiRoute = require('../app/routes/sesiRoutes.js');
+const seatingRoute = require('../app/routes/seatingRoutes.js');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/sesi', sesiRoute);
+app.use('/seating', seatingRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
