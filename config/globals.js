2ec96c5c6f33c2e108ec46a4fd78ab169044e2fe
borednuna <hanunshaka02@gmail.com>
2023-04-04 18:46:10 +0700

    feat: reconfigure port for production

--- config/globals.js
+++ config/globals.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const db_name = process.env.DB_NAME;
 const username = process.env.DB_USER;
 const password = process.env.PASSWORD;
-const port = process.env.PORT;
+const port = process.env.DB_PORT;
 const host = process.env.HOST;
 
 module.exports = { db_name, username, password, port, host };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 80 || 443 || 3000;
+const port = process.env.PORT || 3000;
 
 const sesiRoute = require("./app/routes/sesiRoutes.js");
 const seatingRoute = require("./app/routes/seatingRoutes.js");
