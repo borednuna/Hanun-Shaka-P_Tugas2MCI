e7abb932245079f207335f82ddec8bacbc5881de
borednuna <hanunshaka02@gmail.com>
2023-04-07 00:56:19 +0700

    feat: refactor bridge table

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sesiRoute = require("./app/routes/sesiRoutes.js");
 const seatingRoute = require("./app/routes/seatingRoutes.js");
 const userRoute = require("./app/routes/userRoutes.js");
 const pembayaranRoute = require("./app/routes/pembayaranRoutes.js");
-const detail_sesiRoute = require("./app/routes/detailBookingRoutes.js");
+const detailBookingRoute = require("./app/routes/detailBookingRoutes.js");
 const bookingRoute = require("./app/routes/bookingRoutes.js");
 
 app.use(cors());
@@ -18,7 +18,7 @@ app.use("/sesi", sesiRoute);
 app.use("/seating", seatingRoute);
 app.use("/user", userRoute);
 app.use("/pembayaran", pembayaranRoute);
-app.use("/detail_sesi", detail_sesiRoute);
+app.use("/detail_booking", detailBookingRoute);
 app.use("/booking", bookingRoute);
 
 app.get('/', (req, res) => res.send('Repository for project Backend MCI 2023 => https://github.com/borednuna/Hanun-Shaka-P_Tugas2MCI'));
